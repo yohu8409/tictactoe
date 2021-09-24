3705a7cd4622872d98ae3078b38209248e14b3b7
yohu8409 <yohu8409@colorado.edu>
2021-09-24 16:12:22 -0600

    testing

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -18,15 +18,6 @@ void CreateBoard(std::string matrix[3][3]){
   }
 }
 
-void DisplayBoard(string matrix[3][3]){
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 3; j++){
-      std::cout << matrix[i][j] << " ";
-    }
-    std::cout << std::endl;
-  }
-}
-
 int main(){
   std::string matrix[3][3];
   CreateBoard(matrix);
