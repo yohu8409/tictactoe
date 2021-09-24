8f0565085536908534a7f7eb60ec64d7c88dc95c
yohu8409 <yohu8409@colorado.edu>
2021-09-24 15:56:00 -0600

    display function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <string>
 
-
+void DisplayBoard(std::string matrix[3][3]){
+  for(int a = 0; a < 3; i++){
+    for(int b = 0; b < 3; j++){
+      std::cout<<matrix[a][b]<< " ";
+    }
+    std::cout<<std::endl;
+  }
+}
 
 void CreateBoard(std::string matrix[3][3]){
   for(int i = 0; i < 3; i++){
@@ -14,4 +21,5 @@ void CreateBoard(std::string matrix[3][3]){
 int main(){
   std::string matrix[3][3];
   CreateBoard(matrix);
+  DisplayBoard(matrix);
 }
